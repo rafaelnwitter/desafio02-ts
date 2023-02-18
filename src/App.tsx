9b182a822594287efdb99e29e271d91e7d5a7c0e
Rafael Witt <rafawitt@hotmail.com>
2023-02-17 23:11:45 -0300

    feat: create test footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { 
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
-  Button
-} from '@chakra-ui/react'
-import { login } from './services/login';
+import { Login } from './components/Card';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <ChakraProvider>
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
-          <Center>
-            <h1>Faça o login</h1>
-          </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Center>
-            <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
-              Button
-            </Button>
-          </Center>
-        </Box>
-      </Box>
-    </ChakraProvider>
+    <>
+    <Header />
+    <Login />
+    <Footer />
+    </>
+
   );
 }
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export const Footer  = () => {
+  return(
+    <div className='footer'>
+      <h1>teste</h1>
+    </div>
+  )
+}
