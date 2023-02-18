19be8b691378a4f2d47c7fc3ca38fb849032ff25
Rafael Witt <rafawitt@hotmail.com>
2023-02-17 23:11:13 -0300

    refactor: create components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,6 @@
+import { Box, ChakraProvider, Center, Input, Button } from "@chakra-ui/react"
+import { login } from "../services/login"
+
 interface ICard {
   id: number,
   paragraph: string,
@@ -11,5 +14,26 @@ export const Card = ({ id, paragraph, details }: ICard) => {
       <p>{paragraph}</p>
       <p>{details}</p>
     </div>
+  )
+}
+
+export const Login = () =>  {
+  return(
+    <ChakraProvider>
+          <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+            <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='10px' >
+              <Center>
+                <h1>Faça o login</h1>
+              </Center>
+              <Input placeholder="email" />
+              <Input placeholder="password" />
+              <Center>
+                <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
+                  Button
+                </Button>
+              </Center>
+            </Box>
+          </Box>
+    </ChakraProvider>
   )
 }
